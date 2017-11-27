dfa6e6932039993ecc7b292eafe44c415aeee323
Cassia P Caris <cassia.caris@concrete.com.br>
2017-11-27 14:17:47 -0200

    segund commit

--- calculadora.rb
+++ calculadora.rb
@@ -8,26 +8,26 @@ puts "3 Divisao"
 puts "4 Multiplicacao"
 
 puts "Digite um numero?"
-n1 = gets.chomp().to_i
+n1 = gets.chomp().to_f
 
 puts "Digite o Numero da Operacao escolhida?"
 operacao = gets.chomp().to_i
 
 puts "Digite o segundo numero?"
-n2 = gets.chomp().to_i
+n2 = gets.chomp().to_f
 
 if operacao == 1
 	sum = n1 + n2
 	puts "#{n1} + #{n2} = #{sum}"
 elsif operacao == 2
-	menos = n1 - n2
+	menos = (n1 - n2) * -1 
 	puts "#{n1} - #{n2} = #{menos}"
 elsif operacao == 3
 	if n2 != 0
 		div = n1 / n2
-		puts "#{n1} / #{n2} = #{div}"
+		puts "#{n1} / #{n2} = #{div.round(2)}"
 	else
-		puts "Divisao anulada - Nao tem divisao por 0"
+		puts "Divisao anulada - Nao tem divisao por 0!"
 	end
 elsif operacao == 4
 	multi = n1 * n2
