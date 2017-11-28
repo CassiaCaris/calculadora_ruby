b3853e3965bdb9b9cf3d3606453c170f7ec75239
Cassia P Caris <cassia.caris@concrete.com.br>
2017-11-28 16:55:50 -0200

    calc2 commit

---
+++ calculadora_rspec/README.md
@@ -0,0 +1,16 @@
+Com base no desenvolvimento da calculadora.rb
+Foi realizada alterações no desenvolvimento uma calculadora com os criterios:
+
+- somar, subtrair, multiplicar e dividir
+- tratar divisão por 0
+- tratar números negativos
+- decimais até 2 casas
+- realizando o teste unitário (http://rspec.info/)
+
+para executar o arquivo calculadora2.rb para executar os comandos da calculadora:
+
+ruby calculadora2.rb
+
+para gerar o relatorio com os testes unitários:
+
+rspec unit.rb --format htm --out relatorio.html
--- calculadora_rspec/calculadora2.rb
+++ calculadora_rspec/calculadora2.rb
@@ -4,13 +4,11 @@ class Calculadora
 	def adicao (n1, n2)
 		@res =	(n1 + n2)
 		return @res
-		puts "#{n1} + #{n2} = @res"
 	end
 
 	def subtracao(n1, n2)
 		@res =	(n1 - n2) * -1 
 		return @res
-		puts "#{n1} - #{n2} = @res"
 	end
 	
 
@@ -18,17 +16,14 @@ class Calculadora
 		if n2 != 0
 			@res =	(n1 / n2)
 			return @res.round(3)
-			puts "#{n1} / #{n2} = @res.round(3)"
 		else
 			return "Divisao Invalida!"
-			puts "Divisão Invalida!"
 		end
 	end
 
 	def multiplicacao(n1, n2)
 		@res =	(n1 * n2) 
 		return @res
-		puts "#{n1} / #{n2} = @res"
 	end
 end
 
