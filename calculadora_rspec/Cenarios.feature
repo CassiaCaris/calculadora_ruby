89bccc5aca376d4961124998c6f967f0c5f60e0c
Cassia P Caris <cassia.caris@concrete.com.br>
2017-12-04 15:57:09 -0200

    alteracao_nome

---
+++ calculadora_rspec/Cenarios.feature
@@ -0,0 +1,9 @@
+# language: pt
+
+Funcionalidade: Desenvolvimento de uma Calculadora
+
+Cenario: Realizar a busca do restaurente preferido
+Dado que esteja na pagina inicial do Meat
+Quando pesquisa o seu restaurente preferido na tela com a listagem dos restaurantes
+Entao visualizando o Perfil do Resturante Preferido
+E valida o cardapio on-line
--- calculadora_rspec/unit.rb
+++ calculadora_rspec/unit.rb
@@ -1,7 +1,8 @@
-require_relative "calculadora2"
+require_relative "calculadora"
 
 
 describe Calculadora do
+  #cenario de teste com numeros positivos
   it "Soma de 20 mais 20 deve ser 40" do
     calculo = Calculadora.new()
     # Act
@@ -10,6 +11,7 @@ describe Calculadora do
     expect(resultado).to eq(40)
   end
 
+  #cenario de teste com numeros positivos
   it "Subtracao de 20 menos 45 deve ser 25" do
     calculo = Calculadora.new()
     # Act
@@ -17,16 +19,18 @@ describe Calculadora do
     # Assert
     expect(resultado).to eq(25.0)
   end
-  
-  it "Subtracao de 10 dividido 3 deve ser 3" do
+
+  #cenario de teste com numeros positivos  
+  it "Divisao racao de 11 dividido 3 deve ser 3.0" do
     calculo = Calculadora.new()
     # Act
-    resultado = calculo.divisao(10, 4)
+    resultado = calculo.divisao(11, 3)
     # Assert
-    expect(resultado).to eq(2.0)
+    expect(resultado).to eq(3.0)
   end
 
-  it "Subtracao de 10 dividido 0 deve ser Divisao Invalida!" do
+  #cenario de teste com divisão por 0 
+  it "Divisao de 10 dividido 0 deve ser Divisao Invalida!" do
     calculo = Calculadora.new()
     # Act
     resultado = calculo.divisao(10, 0)
@@ -34,12 +38,49 @@ describe Calculadora do
     expect(resultado).to eq("Divisao Invalida!")
   end
 
-    it "Subtracao de 2 multiplica 10 deve ser 20" do
+  #cenario de teste com numeros positivos
+  it "Multiplicacao de 2 multiplica 10 deve ser 20" do
     calculo = Calculadora.new()
     # Act
     resultado = calculo.multiplicacao(2, 10)
     # Assert
     expect(resultado).to eq(20)
+  end
+
+  #cenario de teste com numeros negativos
+  it "Soma de -10 mais 2 deve ser -8" do
+    calculo = Calculadora.new()
+    # Act
+    resultado = calculo.adicao(-10, 2)
+    # Assert
+    expect(resultado).to eq(-8)
+  end
+
+   #cenario de teste com numeros negativos
+   it "Subtracao de -10 menos 2 deve ser 12" do
+    calculo = Calculadora.new()
+    # Act
+    resultado = calculo.subtracao(-10, 2)
+    # Assert
+    expect(resultado).to eq(12)
+  end
+
+  #cenario de teste com numeros negativos
+  it "Divisao de -3 dividido -3 deve ser 1" do
+    calculo = Calculadora.new()
+    # Act
+    resultado = calculo.divisao(-3, -3)
+    # Assert
+    expect(resultado).to eq(1)
+  end
+
+  #cenario de teste com numeros negativos
+   it "Multiplicacao de -1 multiplica 2 deve ser -2" do
+    calculo = Calculadora.new()
+    # Act
+    resultado = calculo.multiplicacao(-1, 2)
+    # Assert
+    expect(resultado).to eq(-2)
   end
 
 end
